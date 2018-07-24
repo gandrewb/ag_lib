3fc0cb1e4379ccd3672edb1cc31e41783ad122b2
Andy Griffin <griffinandyb@gmail.com>
2018-07-23 17:50:46 -0700

    commented out nonfunctioning parallax object

--- js/ag.js
+++ js/ag.js
@@ -4,7 +4,7 @@ var AG = function(){
 	this.version = '1.0.0';
 	
 	this.Ajax = require('./modules/Ajax');
-	this.Parallax = require('./modules/Parallax');
+	//this.Parallax = require('./modules/Parallax');
 	this.Url = require('./modules/Url');
 };
 
