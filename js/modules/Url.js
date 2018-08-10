79e89c95ab1d41577109a0763e9c32983f1e7ccd
Andy Griffin <griffinandyb@gmail.com>
2018-08-10 13:33:29 -0700

    Adding URL module

--- js/modules/Url.js
+++ js/modules/Url.js
@@ -17,13 +17,74 @@ var Url = function(){
 	var get_vars = window.location.href.split('?');
 	if(get_vars.length>1){
 		get_vars = get_vars[1].split('&');
-		for(i in get_vars){
+		for(var i in get_vars){
 			var kv = get_vars[i].split('=');
 			this.info.vars[kv[0]] = kv[1];
 		}
 	}
+};
+
+var proto = Url.prototype;
+
+
+
+
+/*
+{ 
+	vars: [{key: "foo", val: "bar"}],
+	title: ''
+	history: true
+}
+*/
+
+proto.addVariables = function(options) {
+	options.vars    = options.vars || [];
+	options.title   = options.title || '';
+	options.history = options.history !== false;
 	
-	return this.info;
+	for(var i=0, len=options.vars.length; i<len; i++) {
+		var newvar = options.vars[i];
+		this.info.vars[newvar.key] = newvar.val;
+	}
+	
+	var newpath = this._assembleURL(false);
+	
+	if(options.history) {
+		window.history.pushState('object or string', options.title, newpath);
+	} else {
+		window.history.replaceState('object or string', options.title, newpath);
+	}
+};
+
+proto.getInfo = function() { return this.info; };
+
+
+
+proto._assembleURL = function(includehost) {
+	
+	var vars = '';
+	var hash = '';
+	var ct = 0;
+	
+	for(var key in this.info.vars){
+		if(this.info.vars[key]) {
+			var sep = (ct>0) ? '&' : '?';
+			vars += sep + key + '=' + this.info.vars[key];
+			ct++;
+		}
+	}
+	
+	if(this.info.target) {
+		hash = '#' + this.info.target;
+	}
+	
+	var newpath = '/' + this.info.path.join('/') + vars + hash;
+	
+	if(includehost) {
+		return this.info.protocol + '//' + this.info.hosts.join('.') + newpath;
+	} else {
+		return newpath;
+	}
 };
 
 module.exports = Url;
